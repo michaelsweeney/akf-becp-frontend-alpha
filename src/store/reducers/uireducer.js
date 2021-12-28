d69c2f65b4299c215e9add5ff0be5589e7cc2c59
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2021-12-28 16:45:14 -0500

    d

--- src/store/reducers/uireducer.js
+++ src/store/reducers/uireducer.js
@@ -1,55 +1,10 @@
-const initialState = {
-  isLoadModalOpen: false,
-  isAboutModalOpen: true,
-  isLoadedSummaryModalOpen: false,
-  activeAccordionKey: "property_types",
-  activeView: "lineplot_thresholds", // lineplot_thresholds, lineplot_payments, tabular
-};
+const initialState = {};
 
 export default function uiReducer(state = initialState, action) {
   switch (action.type) {
-    case "SET_IS_LOAD_MODAL_OPEN": {
+    case "DO_SOMETHING": {
       return {
         ...state,
-        isLoadModalOpen: action.payload,
-      };
-    }
-    case "SET_IS_ABOUT_MODAL_OPEN": {
-      return {
-        ...state,
-        isAboutModalOpen: action.payload,
-      };
-    }
-    case "SET_IS_LOADED_SUMMARY_MODAL_OPEN": {
-      return {
-        ...state,
-        isLoadedSummaryModalOpen: action.payload,
-      };
-    }
-    case "SET_WINDOW_DIMENSIONS": {
-      return {
-        ...state,
-        dims: action.payload,
-      };
-    }
-    case "TOGGLE_ACCORDION": {
-      let active_key;
-      if (action.payload === state.activeAccordionKey) {
-        active_key = "property_types";
-      } else {
-        active_key = action.payload;
-      }
-
-      return {
-        ...state,
-        activeAccordionKey: active_key,
-      };
-    }
-
-    case "SET_ACTIVE_VIEW": {
-      return {
-        ...state,
-        activeView: action.payload,
       };
     }
 
