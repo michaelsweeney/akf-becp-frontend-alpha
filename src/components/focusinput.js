651dc1239d37320cf29d14a2227183f91527b2a9
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2022-04-09 20:38:18 -0400

    fix focus

--- src/components/focusinput.js
+++ src/components/focusinput.js
@@ -2,25 +2,29 @@ import { Input } from "@mui/material";
 import { useEffect, useState } from "react";
 const FocusInput = (props) => {
   const { callback, value, inputType } = props;
-  // console.log(value);
-  let stateValue = value;
-  const [inputValue, setInputValue] = useState(stateValue);
 
-  // useEffect(() => {
-  // console.log(inputValue, stateValue);
-  // setInputValue(stateValue);
-  // callback(stateValue);
-  // }, [stateValue]);
+  let [hardValue, setHardValue] = useState(value);
+
+  let [inputValue, setInputValue] = useState(value);
+
+  const handleInput = (v) => {
+    setInputValue(v);
+  };
+
+  useEffect(() => {
+    if (value != inputValue) {
+      setInputValue(value);
+    }
+  }, [value]);
 
   return (
     <Input
       type={inputType}
       onFocus={(e) => {}}
       onChange={(e) => {
-        setInputValue(e.target.value);
+        handleInput(e.target.value);
       }}
       onBlur={(e) => {
-        // console.log(e.target.value, stateValue);
         callback(e.target.value);
       }}
       value={inputValue}
